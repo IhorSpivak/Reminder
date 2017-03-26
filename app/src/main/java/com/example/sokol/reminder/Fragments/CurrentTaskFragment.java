232e1e710d586ced9cc72932af7c03b88e0de70a
Spivakov Ihor <haterkyiv@gmail.com>
2017-03-26 20:04:36 +0300

    Add Custom Adapter, delete SplashFragment

--- app/src/main/java/com/example/sokol/reminder/Fragments/CurrentTaskFragment.java
+++ app/src/main/java/com/example/sokol/reminder/Fragments/CurrentTaskFragment.java
@@ -3,11 +3,15 @@ package com.example.sokol.reminder.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.sokol.reminder.CurrentTaskAdapter;
 import com.example.sokol.reminder.R;
+import com.example.sokol.reminder.model.ModelTask;
 
 
 /**
@@ -16,6 +20,12 @@ import com.example.sokol.reminder.R;
 public class CurrentTaskFragment extends Fragment {
 
 
+    private RecyclerView rvCurrentTask;
+    private RecyclerView.LayoutManager layoutManager;
+
+
+    private CurrentTaskAdapter adapter;
+//
     public CurrentTaskFragment() {
         // Required empty public constructor
     }
@@ -24,8 +34,40 @@ public class CurrentTaskFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_current_task, container, false);
-    }
 
+        View rootView = inflater.inflate(R.layout.fragment_current_task, container, false);
+
+        rvCurrentTask = (RecyclerView) rootView.findViewById(R.id.rvCurrentTasks);
+
+        layoutManager = new LinearLayoutManager(getActivity());
+
+        rvCurrentTask.setLayoutManager(layoutManager);
+
+        adapter = new CurrentTaskAdapter();
+        rvCurrentTask.setAdapter(adapter);
+
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+//
+    public void addTask(ModelTask newTask) {
+        int position = -1;
+
+        for (int i = 0; i < adapter.getItemCount(); i ++) {
+            if (adapter.getItem(i).isTask()) {
+                ModelTask task = (ModelTask) adapter.getItem(i);
+                if (newTask.getDate() < task.getDate()) {
+                    position = i;
+                    break;
+                }
+            }
+        }
+
+        if (position != -1) {
+            adapter.addItem(position, newTask);
+        } else {
+            adapter.addItem(newTask);
+        }
+    }
 }
+//
